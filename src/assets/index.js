cb78cca1bb6004f6479aca3c4eaf011b9eccd422
GOLWEYNE <32921055+GOLWEYNE@users.noreply.github.com>
2026-08-24 02:15:33 +0700

    Import Alan International School dashboard image

--- src/assets/index.js
+++ src/assets/index.js
@@ -29,8 +29,10 @@ import tesla from "./company/tesla.png";
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
+import alanschool from "./alanschool.jpg";
 
 export {
+    alanschool,
   logo,
   backend,
   creator,
